cd62fc5a99d6437ecdeb7e5a848f681d3a4719dd
animesh33-ctrl <paluianimesh31@gmail.com>
2026-06-12 20:55:17 +0530

    Refactor form component and update title for consistency in react_pr

--- react-pr/Form_Creation/src/App.jsx
+++ react-pr/Form_Creation/src/App.jsx
@@ -1,10 +1,19 @@
 import Form from "./components/Form.jsx";
 import "./App.css";
+import { useState } from "react";
 
 const App = () => {
+  const [data, setData] = useState([]);
+
+  const handleData = (value) => {
+    setData((prev) => {
+      return [...prev, value];
+    });
+  };
+
   return (
     <>
-      <Form />
+      <Form handleData={handleData} />
     </>
   );
 };
--- react-pr/Form_Creation/src/components/Form.jsx
+++ react-pr/Form_Creation/src/components/Form.jsx
@@ -1,7 +1,6 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 
-const Form = () => {
+const Form = ({ handleData }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -9,127 +8,75 @@ const Form = () => {
     confirm: "",
   });
 
-  const [data, setData] = useState([]);
-
-  const handleOnChange = (event) => {
-    console.log(event);
-    let newData = { ...formData, [event.target.name]: event.target.value };
-    setFormData(newData);
+  const handleInput = (event) => {
+    setFormData((prev) => ({
+      ...prev,
+      [event.target.name]: event.target.value,
+    }));
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    if (formData.password === formData.confirm) {
-      setData([
-        ...data,
-        {
-          name: formData.name,
-          email: formData.email,
-          password: formData.password,
-        },
-      ]);
-      alert("Form Submiited Successfully");
-      setFormData({
-        name: "",
-        email: "",
-        password: "",
-        confirm: "",
-      });
-    } else {
-      alert("Password Mismatch!!");
-    }
+    handleData(formData);
+    setFormData({
+      name: "",
+      email: "",
+      password: "",
+      confirm: "",
+    });
   };
 
   return (
-    <div className="min-h-screen w-full bg-[linear-gradient(135deg,#312e81,#6d28d9,#db2777)] flex justify-center items-center flex-wrap">
-      <div
-        className="w-[90%] sm:w-[75%] md:w-[55%] xl:w-[28%] min-h-[60vh] bg-white/10 border border-white/20 rounded-3xl 
-      flex flex-col items-center p-6 md:p-8 backdrop-blur-xl shadow-2xl"
-      >
-        <form id="myForm" className="space-y-3 w-full " onSubmit={handleSubmit}>
-          <h2 className="text-center text-white text-3xl md:text-4xl font-bold tracking-wide mt-7">
-            Registration
-          </h2>
-          <div className="w-full      flex flex-col gap-2">
-            <label
-              className="text-base md:text-lg text-gray-200"
-              htmlFor="name"
-            >
-              Name
-            </label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              value={formData.name}
-              onChange={handleOnChange}
-              placeholder="Enter your name"
-              className="w-full rounded-xl border border-white/20 bg-white/10 px-4 py-3
-                text-white placeholder:text-gray-300 outline-none"
-            />
-          </div>
-          <div className="w-full      flex flex-col gap-2">
-            <label
-              className="text-base md:text-lg text-gray-200"
-              htmlFor="email"
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={formData.email}
-              onChange={handleOnChange}
-              placeholder="Enter your email"
-              className="w-full rounded-xl border border-white/20 bg-white/10 px-4 py-3
-                text-white placeholder:text-gray-300 outline-none"
-            />
-          </div>
-          <div className="w-full      flex flex-col gap-2">
-            <label
-              className="text-base md:text-lg text-gray-200"
-              htmlFor="password"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              value={formData.password}
-              onChange={handleOnChange}
-              placeholder="Enter your Password"
-              className="w-full rounded-xl border border-white/20 bg-white/10 px-4 py-3
-                text-white placeholder:text-gray-300 outline-none"
-            />
-          </div>
-          <div className="w-full      flex flex-col gap-2">
-            <label
-              className="text-base md:text-lg text-gray-200"
-              htmlFor="confirm"
-            >
-              Confirm Password
-            </label>
-            <input
-              type="password"
-              id="confirm"
-              name="confirm"
-              value={formData.confirm}
-              onChange={handleOnChange}
-              placeholder="Confirm your Password"
-              className="w-full rounded-xl border border-white/20 bg-white/10 px-4 py-3
-                text-white placeholder:text-gray-300 outline-none"
-            />
-          </div>
-          <button
-            className="w-full bg-indigo-500 py-3 rounded-xl hover:bg-indigo-700 hover:text-white transition duration-300 hover:-translate-y-1
-                cursor-pointer hover:scale-[1.02] text-gray-100 mt-3"
-          >
-            Submit
-          </button>
-        </form>
-      </div>
+    <div>
+      <form action="" onSubmit={handleSubmit}>
+        <div>
+          <label>Name</label>
+          <input
+            type="text"
+            placeholder="Enter your name"
+            name="name"
+            required
+            value={formData.name}
+            onChange={handleInput}
+          />
+        </div>
+        <div>
+          <label>Email</label>
+          <input
+            type="text"
+            placeholder="Enter your email"
+            name="email"
+            required
+            value={formData.email}
+            onChange={handleInput}
+          />
+        </div>
+        <div>
+          <label>Password</label>
+          <input
+            type="password"
+            placeholder="Enter your password"
+            name="password"
+            required
+            value={formData.password}
+            onChange={handleInput}
+          />
+        </div>
+        <div>
+          <label>Confirm Password</label>
+          <input
+            type="password"
+            placeholder="Enter your password again"
+            name="confirm"
+            required
+            value={formData.confirm}
+            onChange={handleInput}
+          />
+        </div>
+        <div>
+          <button>Submit</button>
+        </div>
+      </form>
     </div>
   );
 };
